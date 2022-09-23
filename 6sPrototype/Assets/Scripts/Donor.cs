8a90c43e7f9137817ccdd30a3b031c96124babd8
abigailfloyd <abigailf@usc.edu>
2022-09-23 10:22:53 -0700

    Recommitting files

---
+++ 6sPrototype/Assets/Scripts/Donor.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Donor : MonoBehaviour
+{
+    public StampSystem.StampType correctStamp;
+    List<Color> colors = new List<Color>();
+    public bool isStamped = false;
+    public bool draggedOverFolder = false;
+    [SerializeField] StampSystem stampSystem;
+    [SerializeField] GameObject stampImage;
+    [SerializeField] GameObject coloredSquare;
+    [SerializeField] GameObject speechBubble;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        colors.Add(Color.blue);
+        colors.Add(Color.red);
+        colors.Add(Color.black);
+        colors.Add(Color.green);
+        colors.Add(Color.yellow);
+
+        SetCorrectStamp();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetCorrectStamp()
+    {
+        int randomNum = Random.Range(0, 5);
+        if (randomNum ==  0)
+        {
+            correctStamp = StampSystem.StampType.Identify;
+        }
+        else if (randomNum == 1)
+        {
+            correctStamp = StampSystem.StampType.Introduce;
+        }
+        else if (randomNum == 2)
+        {
+            correctStamp = StampSystem.StampType.Invest;
+        }
+        else if (randomNum == 3)
+        {
+            correctStamp = StampSystem.StampType.Inform;
+        }
+        else if (randomNum == 4)
+        {
+            correctStamp = StampSystem.StampType.ReInvest;
+        }
+
+
+        int randomColorInt = Random.Range(0, 5);
+        coloredSquare.GetComponent<Image>().color = colors[randomColorInt];
+        
+    }
+
+  
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Processing Folder" && isStamped)
+        {
+            draggedOverFolder = true;
+            stampSystem.CheckStamp();
+            if (stampSystem.interestButtonTutorialDone)
+            {
+                speechBubble.SetActive(false);
+            }
+        }
+    }
+
+    void onTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Processing Folder")
+        {
+            draggedOverFolder = false;
+        }
+    }
+}
---
+++ 6sPrototype/Assets/Scripts/StampSystem.cs
@@ -0,0 +1,251 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class StampSystem : MonoBehaviour
+{
+
+    public enum StampType 
+    {
+        Identify,
+        Introduce, 
+        Interest, 
+        Invest, 
+        Inform, 
+        ReInvest
+    }
+
+    private StampType currentStamp;
+    public StampType correctStamp;
+
+    [SerializeField] GameObject identifyButton;
+    [SerializeField] GameObject introduceButton;
+    [SerializeField] GameObject interestButton;
+    [SerializeField] GameObject investButton;
+    [SerializeField] GameObject informButton;
+    [SerializeField] GameObject reInvestButton;
+
+    public List<Button> buttons = new List<Button>();
+
+    [SerializeField] Donor donor;
+    [SerializeField] TMP_Text resultText;
+    [SerializeField] TMP_Text points;
+    [SerializeField] Button nextButton;
+
+    [SerializeField] Sprite emptyFolderImage;
+    [SerializeField] Sprite folderImage;
+    [SerializeField] GameObject processingFolder;
+    [SerializeField] SpeechBubble speechBubble;
+    private int numDonorsComplete = 0;
+
+    [SerializeField] GameObject stampedImage;
+
+    public int playerPoints = 0;
+
+    public bool interestButtonTutorialDone = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PopulateButtonText();
+        PopulateButtonClicked();
+        correctStamp = donor.correctStamp;
+
+        nextButton.GetComponent<Button>().onClick.AddListener(delegate { PressNextButton(); });
+
+        ShowInitButtons();
+        ShowInitButtons();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+    void PopulateButtonClicked()
+    {
+        foreach (Button button in buttons)
+        {
+            button.GetComponent<Button>().onClick.AddListener(delegate { SetCurrentStamp(button); });
+        }
+    }
+
+    void PopulateButtonText()
+    {
+        identifyButton.GetComponentInChildren<TMP_Text>().text = "Identify";
+        introduceButton.GetComponentInChildren<TMP_Text>().text = "Introduce";
+        interestButton.GetComponentInChildren<TMP_Text>().text = "Interest";
+        investButton.GetComponentInChildren<TMP_Text>().text = "Invest";
+        informButton.GetComponentInChildren<TMP_Text>().text = "Inform";
+        reInvestButton.GetComponentInChildren<TMP_Text>().text = "Re-Invest";
+
+        buttons.Add(identifyButton.GetComponentInChildren<Button>());
+        buttons.Add(introduceButton.GetComponentInChildren<Button>());
+        buttons.Add(interestButton.GetComponentInChildren<Button>());
+        buttons.Add(investButton.GetComponentInChildren<Button>());
+        buttons.Add(informButton.GetComponentInChildren<Button>());
+        buttons.Add(reInvestButton.GetComponentInChildren<Button>());
+
+    }
+
+    public void CheckStamp()
+    {
+        if (currentStamp == correctStamp)
+        {
+            if (currentStamp != StampType.Interest)
+            {
+                playerPoints += 200;
+                resultText.text = "Correct!";
+                ShowNextButton();
+                numDonorsComplete++;
+                if (numDonorsComplete == 1)
+                {
+                    processingFolder.GetComponent<Image>().sprite = folderImage;
+                }
+            }
+        }
+        else
+        {
+            playerPoints -= 100;
+            resultText.text = "Incorrect :(";
+            ShowNextButton();
+            numDonorsComplete++;
+            if (numDonorsComplete == 1)
+            {
+                processingFolder.GetComponent<Image>().sprite = folderImage;
+            }
+        }
+
+     
+        points.text = "Points: " + playerPoints.ToString();
+        donor.gameObject.SetActive(false);
+
+
+    }
+    
+    void ShowNextButton()
+    {
+        nextButton.gameObject.SetActive(true);
+        HideAllButtons();
+    }
+
+    void PressNextButton()
+    {
+        donor.gameObject.SetActive(true);
+        donor.gameObject.GetComponent<DraggableUI>().ResetPosition();
+        donor.isStamped = false;
+        stampedImage.gameObject.SetActive(false);
+        resultText.text = "";
+        nextButton.gameObject.SetActive(false);
+        donor.SetCorrectStamp();
+        ShowInitButtons();
+        correctStamp = donor.correctStamp;
+        foreach (Button button in buttons)
+        {
+            button.gameObject.GetComponent<DraggableUI>().ResetPosition();
+            button.interactable = false;
+        }
+    }
+
+    void ShowInterestButtons()
+    {
+        identifyButton.gameObject.SetActive(false);
+        introduceButton.gameObject.SetActive(false);
+        interestButton.gameObject.SetActive(false);
+
+        investButton.gameObject.SetActive(true);
+        informButton.gameObject.SetActive(true);
+        reInvestButton.gameObject.SetActive(true);
+    }
+
+    void ShowInitButtons()
+    {
+        investButton.gameObject.SetActive(false);
+        informButton.gameObject.SetActive(false);
+        reInvestButton.gameObject.SetActive(false);
+
+        identifyButton.gameObject.SetActive(true);
+        introduceButton.gameObject.SetActive(true);
+        interestButton.gameObject.SetActive(true);
+    }
+
+    void HideAllButtons()
+    {
+        identifyButton.gameObject.SetActive(false);
+        introduceButton.gameObject.SetActive(false);
+        interestButton.gameObject.SetActive(false);
+
+        investButton.gameObject.SetActive(false);
+        informButton.gameObject.SetActive(false);
+        reInvestButton.gameObject.SetActive(false);
+    }
+
+    void SetCurrentStamp(Button button)
+    {
+        if (button == identifyButton.GetComponentInChildren<Button>())
+        {
+            currentStamp = StampType.Identify;
+        }
+        else if (button == introduceButton.GetComponentInChildren<Button>())
+        {
+            currentStamp = StampType.Introduce;
+        }
+        else if (button == interestButton.GetComponentInChildren<Button>())
+        {
+            currentStamp = StampType.Interest;
+        }
+        else if (button == investButton.GetComponentInChildren<Button>())
+        {
+            if (!interestButtonTutorialDone)
+            {
+                interestButtonTutorialDone = true;
+                speechBubble.ChangeToText4();
+            }
+            currentStamp = StampType.Invest;
+        }
+        else if (button == informButton.GetComponentInChildren<Button>())
+        {
+            if (!interestButtonTutorialDone)
+            {
+                interestButtonTutorialDone = true;
+                speechBubble.ChangeToText4();
+            }
+            currentStamp = StampType.Inform;
+        }
+        else if (button == reInvestButton.GetComponentInChildren<Button>())
+        {
+             if (!interestButtonTutorialDone)
+            {
+                interestButtonTutorialDone = true;
+                speechBubble.ChangeToText4();
+            }
+            currentStamp = StampType.ReInvest;
+        }
+        if (button != interestButton.GetComponentInChildren<Button>())
+        {
+            Debug.Log("STAMPED");
+            donor.isStamped = true;
+            stampedImage.gameObject.SetActive(true);
+            button.gameObject.GetComponent<DraggableUI>().ResetPosition();
+            if (speechBubble.gameObject.activeInHierarchy)
+            {
+                speechBubble.ChangeToText4();
+            }
+        }
+        else if (button == interestButton.GetComponentInChildren<Button>())
+        {
+            ShowInterestButtons();
+            if (!interestButtonTutorialDone)
+            {
+                speechBubble.ChangeToText3();
+            }
+        }
+
+
+    }
+
+}
