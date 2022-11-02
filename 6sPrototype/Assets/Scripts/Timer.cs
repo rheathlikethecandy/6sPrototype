99c8a1fc136286c8126b7bc05cf4b9b34f0533cc
Ryan Heath <heathlikethecandy@gmail.com>
2022-11-02 13:59:43 -0700

    done?

--- 6sPrototype/Assets/Scripts/Timer.cs
+++ 6sPrototype/Assets/Scripts/Timer.cs
@@ -8,10 +8,14 @@ public class Timer : MonoBehaviour
 {
     public int min = 5;
     public float sec = 0;
+    [SerializeField] GameObject fan;
+    [SerializeField] GameObject phone;
+    [SerializeField] GameObject coffee;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("DoRandom", 5.0f, 4.5f);
     }
 
     // Update is called once per frame
@@ -32,7 +36,40 @@ public class Timer : MonoBehaviour
             gameObject.GetComponent<TMP_Text>().text = min + ":0" + (int)sec;
         }
 
-        
+    }
 
+    public void DoRandom()
+    {
+        int randHazard = (int)Random.Range(0f, 2.999f);
+        if (randHazard == 0)
+        {
+            DoFan();
+        }
+        else if (randHazard == 1)
+        {
+            DoPhone();
+        }
+        else
+        {
+            DoCoffee();
+        }
+    }
+
+    public void DoFan()
+    {
+        Debug.Log("fan\n");
+        fan.GetComponent<FanScript>().Blow();
+    }
+
+    public void DoPhone()
+    {
+        Debug.Log("phone\n");
+        phone.GetComponent<PhoneScript>().Call();
+    }
+
+    public void DoCoffee()
+    {
+        Debug.Log("coffee\n");
+        coffee.GetComponent<CoffeeScript>().Fill();
     }
 }
